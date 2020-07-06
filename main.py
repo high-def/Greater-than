37bd06d73128e6804c91cab3c73b1412f6e82344
highdef1 <ryancallebaut@gmail.com>
2020-07-06 17:17:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+x = input("Enter a number: ")
+if int(x) > 10:
+  print("Your number is greater than 10")
+elif int(x) == 10:
+  print("Your number is equal to 10")
+else:
+  print("Your number is less than 10")
